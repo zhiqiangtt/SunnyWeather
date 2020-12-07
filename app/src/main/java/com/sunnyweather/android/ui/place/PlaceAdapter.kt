91147653e8cc66a8dec364d306ec06b49ab816a9
TZQ <1737624959@qq.com>
2020-12-07 10:09:34 +0800

    实现搜索全球城市数据功能。

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceAdapter.kt
@@ -1,17 +1,26 @@
 package com.sunnyweather.android.ui.place
 
+
+
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
+
+
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+
 import androidx.recyclerview.widget.RecyclerView
 import com.sunnyweather.android.R
+
+
 import com.sunnyweather.android.logic.model.Place
+import com.sunnyweather.android.ui.weather.WeatherActivity
 
 
 class PlaceAdapter(private val fragment: Fragment, private val placeList: List<Place>) :
-    RecyclerView.Adapter<PlaceAdapter.ViewHolder>(){
+    RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val placeName: TextView = view.findViewById(R.id.placeName)
@@ -19,8 +28,18 @@ class PlaceAdapter(private val fragment: Fragment, private val placeList: List<P
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item,
-            parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.place_item, parent, false)
+        val holder = ViewHolder(view)
+        holder.itemView.setOnClickListener {
+            val position = holder.adapterPosition
+            val place = placeList[position]
+            val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                putExtra("location_lng", place.location.lng)
+                putExtra("location_lat", place.location.lat)
+                putExtra("place_name", place.name)
+            }
+            fragment.startActivity(intent)
+        }
         return ViewHolder(view)
     }
 
@@ -28,7 +47,11 @@ class PlaceAdapter(private val fragment: Fragment, private val placeList: List<P
         val place = placeList[position]
         holder.placeName.text = place.name
         holder.placeAddress.text = place.address
+
+
     }
 
-    override fun getItemCount() = placeList.size
+    override fun getItemCount(): Int {
+        return placeList.size
+    }
 }
